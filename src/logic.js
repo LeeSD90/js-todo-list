1055a65aecc2c4c48d80ed4173528c5c39fa7d32
LeeSD90 <lee.doyle90@gmail.com>
2018-06-21 13:08:39 +0200

    Add basic project list rendering

--- src/logic.js
+++ src/logic.js
@@ -2,13 +2,21 @@ import projectFactory from './project.js';
 import todoFactory from './todo.js';
 
 const newProject = (name) => {
-    var project = new projectFactory(name);
+    const project = projectFactory(name);
 
     return project;
 }
 
 const newTodo = (title, description, dueDate, priority) => {
-    var todo = new todoFactory(title, description, dueDate, priority);
+    var todo = todoFactory(title, description, dueDate, priority);
 
     return todo;
 }
+
+var projects = [];
+var def = newProject("test");
+projects.push(def);
+def = newProject("anothertest");
+projects.push(def);
+
+export default projects;
--- src/render.js
+++ src/render.js
@@ -1,5 +1,17 @@
+import projects from "./logic.js";
+
 const render = () => {
-    
+    renderProjectList();
+}
+
+const renderProjectList = () => {
+    var view = document.getElementById('project-list');
+    projects.forEach((p) => {
+        var item = document.createElement("div");
+        item.setAttribute("class", "project-item");
+        item.innerHTML = p.name;
+        view.appendChild(item);
+    })
 }
 
 export default render;
