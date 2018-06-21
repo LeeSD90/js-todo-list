cf9a0de6fd270bdedc575e35190e7959b26e8543
LeeSD90 <lee.doyle90@gmail.com>
2018-06-21 14:11:43 +0200

    logic function refactor

--- src/logic.js
+++ src/logic.js
@@ -1,22 +1,20 @@
 import projectFactory from './project.js';
 import todoFactory from './todo.js';
 
-const newProject = (name) => {
+const addNewProject = (name) => {
     const project = projectFactory(name);
 
     return project;
 }
 
-const newTodo = (title, description, dueDate, priority) => {
+const addNewTodo = (title, description, dueDate, priority) => {
     var todo = todoFactory(title, description, dueDate, priority);
 
     return todo;
 }
 
 var projects = [];
-var def = newProject("test");
-projects.push(def);
-def = newProject("anothertest");
+var def = addNewProject("Default");
 projects.push(def);
 
 export default projects;
